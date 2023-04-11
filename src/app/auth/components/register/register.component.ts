057b5c17f868c6f1d0a402cf0b3defe6f5d7224d
Shubham <shubham@Shubhs-BEAST.local>
2023-04-11 18:53:02 +0530

    Login and register form built using reactive forms

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LocalStorageEncryptService } from 'src/app/shared/services/local-storage-encrypt.service';
 
@@ -8,8 +9,22 @@ import { LocalStorageEncryptService } from 'src/app/shared/services/local-storag
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  
+  loader : boolean = false
+  logIn!: string;
+  hide = true;
+  hideRequiredMarker = true;
 
   constructor( private localStore: LocalStorageEncryptService, private router : Router) { }
+
+  form = new FormGroup({    
+    name: new FormControl(""),
+    email: new FormControl(""),
+    phone: new FormControl(""),
+    address: new FormControl(""),
+    unit_number: new FormControl(""),
+    password: new FormControl("")
+  })
 
   ngOnInit(): void {
   }
